d57c22142ab57fbc8e219f94118f55663ea33bef
LucasGomes23 <lucasdeagomes1@gmail.com>
2021-01-06 21:14:14 +0000

    Leason on If-statements.

--- LearningCPP/LearningCPP/LearningCPP.cpp
+++ LearningCPP/LearningCPP/LearningCPP.cpp
@@ -1,43 +1,30 @@
-// LearningCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 #include <string>
 
-int add(int a, int b);
-void say_hello(std::string name);
-/*
-int main() //entry point
-{
-    std::cout << "Result of the calculation: " << std::to_string(pow(2, 8)) << "\n";
-    std::cout << "Hello World!, lucas\n";
-    return 2;
-}
-*/
 
-/*
-argc = 3 
-argv ["hello", "example", "institute"}
-retrned int mean the error of your program 
-*/
-int main(int argc, char** argv)
+int main()
 {
-    say_hello("Lucas");
-    std::cout << "hello world! \n" << "this is a new line \n";
-    std::cout << "number of arguments: " << std::to_string(argc) << "\n";
-    std::cout << " value passed to program at position 1: " << argv[0] << "\n";
-    std::cout << " result of adding 17 and 10: " << std::to_string(add(17, 10)) << "\n";
-    //std::cout << "hello " << argv[1] << "\n";
+    
+    int age = 10;
+
+   std::cout << "the value of age is " << std::to_string(age) << "\n";
+  // check the customer age
+    if (age == 18)
+    {
+        std::cout << "age value is 18 \n";
+    }
+    else if (age == 17)
+    {
+        std::cout << "age value is 17\n";
+    }
+    else
+    {
+        std::cout << "age is not 18 or 17\n";
+    }
+
+    std::cout << "the value of age is " << std::to_string(age) << "\n";
 }
 
-int add(int a, int b)
-{
 
-    return a + b;
-
-}
-
-void say_hello(std::string name)
-{
-    std::cout << "hello " << name << "\n";
-}
