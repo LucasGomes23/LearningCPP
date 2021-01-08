24bd869032d35548d315dc4bd5efa361c77d9935
Maatheus Gomes <matheusgarcia28@gmail.com>
2021-01-08 18:15:31 +0000

    indentation.

--- LearningCPP/LearningCPP/LearningCPP.cpp
+++ LearningCPP/LearningCPP/LearningCPP.cpp
@@ -6,11 +6,10 @@
 
 int main()
 {
-    
     int age = 10;
+    std::cout << "the value of age is " << std::to_string(age) << "\n";
 
-   std::cout << "the value of age is " << std::to_string(age) << "\n";
-  // check the customer age
+    // check the customer age
     if (age == 18)
     {
         std::cout << "age value is 18 \n";
