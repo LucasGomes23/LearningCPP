95a9c46abf939445f485c90be19d2871241355c8
Matheus Gomes <matheusgarcia28@gmail.com>
2021-02-09 20:10:51 +0000

    Fixing logic mistakes.

--- LearningCPP/LearningCPP/LearningCPP.cpp
+++ LearningCPP/LearningCPP/LearningCPP.cpp
@@ -13,17 +13,16 @@ void show_xor(bool a, bool b)
 		<< "b: " << std::to_string(b) << "  "
 		<< "a and b: " << std::to_string((!a && b) || (a && !b)) << "\n";
 }
-void show_not(bool a, bool b)
+void show_not(bool a)
 {
 	std::cout << "a: " << std::to_string(a) << "  "
-		<< "b: " << std::to_string(b) << "  "
-		<< "a and b: " << std::to_string(a && !b) << "\n";
+		<< "not a: " << std::to_string(!a) << "\n";
 }
 void show_or(bool a, bool b)
 {
 	std::cout << "a: " << std::to_string(a) << "  "
 		<< "b: " << std::to_string(b) << "  "
-		<< "a and b: " << std::to_string(!a && b) << "\n";
+		<< "a and b: " << std::to_string(a || b) << "\n";
 }
 int main()
 {
@@ -37,24 +36,22 @@ int main()
 
 	std::cout << "====TRUTH TABLE FOR OR =========\n";
 
-	show_and(false, false);
-	show_and(false, true);
-	show_and(true, false);
-	show_and(true, true);
+	show_or(false, false);
+	show_or(false, true);
+	show_or(true, false);
+	show_or(true, true);
 
 	std::cout << "====TRUTH TABLE FOR XOR =========\n";
 
 
-	show_and(false, false);
-	show_and(false, true);
-	show_and(true, false);
-	show_and(true, true);
+	show_xor(false, false);
+	show_xor(false, true);
+	show_xor(true, false);
+	show_xor(true, true);
 
 	std::cout << "====TRUTH TABLE FOR NOT =========\n";
 
-	show_and(false, false);
-	show_and(false, true);
-	show_and(true, false);
-	show_and(true, true);
+	show_not(false);
+	show_not(true);
 
 }
